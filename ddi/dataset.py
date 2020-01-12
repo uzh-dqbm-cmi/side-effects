efc36937a363e1c8405caf70d3e7e1d5799e765c
orisenbazuru <orisenbazuru@gmail.com>
2020-01-12 18:22:24 +0100

    added neural workflow for DDI model

---
+++ ddi/dataset.py
@@ -0,0 +1,213 @@
+import os
+import numpy as np
+import torch
+from .utilities import ModelScore, ReaderWriter
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import StratifiedKFold, StratifiedShuffleSplit
+from sklearn.utils.class_weight import compute_class_weight
+
+
+class DDIDataTensor(Dataset):
+
+    def __init__(self, X_feat, y):
+        self.X_feat = X_feat  # tensor.float32, (drug pairs, features)
+        # drug interactions
+        self.y = y  # tensor.float32, (drug pairs,)
+        self.num_samples = self.y.size(0)  # int, number of drug pairs
+
+    def __getitem__(self, indx):
+
+        return(self.X_feat[indx], self.y[indx], indx)
+
+    def __len__(self):
+        return(self.num_samples)
+
+
+class PartitionDataTensor(Dataset):
+
+    def __init__(self, ddi_datatensor, partition_ids, dsettype, fold_num):
+        self.ddi_datatensor = ddi_datatensor  # instance of :class:`DDIDataTensor`
+        self.partition_ids = partition_ids  # list of indices for drug pairs
+        self.dsettype = dsettype  # string, dataset type (i.e. train, validation, test)
+        self.fold_num = fold_num  # int, fold number
+        self.num_samples = len(self.partition_ids)  # int, number of docs in the partition
+
+    def __getitem__(self, indx):
+        target_id = self.partition_ids[indx]
+        return self.ddi_datatensor[target_id]
+
+    def __len__(self):
+        return(self.num_samples)
+
+
+def construct_load_dataloaders(dataset_fold, dsettypes, config, wrk_dir):
+    """construct dataloaders for the dataset for one fold
+
+       Args:
+            dataset_fold: dictionary,
+                          example: {'train': <neural.dataset.PartitionDataTensor at 0x1cec95c96a0>,
+                                    'validation': <neural.dataset.PartitionDataTensor at 0x1cec95c9208>,
+                                    'test': <neural.dataset.PartitionDataTensor at 0x1cec95c9240>,
+                                    'class_weights': tensor([0.6957, 1.7778])
+                                   }
+            dsettype: list, ['train', 'validation', 'test']
+            config: dict, {'batch_size': int, 'num_workers': int}
+            wrk_dir: string, folder path
+    """
+
+    # setup data loaders
+    data_loaders = {}
+    epoch_loss_avgbatch = {}
+    epoch_loss_avgsamples = {}
+    flog_out = {}
+    score_dict = {}
+    class_weights = {}
+    for dsettype in dsettypes:
+        if(dsettype == 'train'):
+            shuffle = True
+            class_weights[dsettype] = dataset_fold['class_weights']
+        else:
+            shuffle = False
+            class_weights[dsettype] = None
+        data_loaders[dsettype] = DataLoader(dataset_fold[dsettype],
+                                            batch_size=config['batch_size'],
+                                            shuffle=shuffle,
+                                            num_workers=config['num_workers'])
+
+        epoch_loss_avgbatch[dsettype] = []
+        epoch_loss_avgsamples[dsettype] = []
+        score_dict[dsettype] = ModelScore(0, 0.0, 0.0, 0.0, 0.0, 0.0)  # (best_epoch, auc, aupr, f1, precision, recall)
+        if(wrk_dir):
+            flog_out[dsettype] = os.path.join(wrk_dir, dsettype + ".log")
+        else:
+            flog_out[dsettype] = None
+
+    return (data_loaders, epoch_loss_avgbatch, epoch_loss_avgsamples, score_dict, class_weights, flog_out)
+
+def preprocess_features(feat_fpath):
+    X_fea = np.loadtxt(feat_fpath,dtype=float,delimiter=",")
+    r, c = np.triu_indices(len(X_fea),1) # take indices off the diagnoal by 1
+    return np.concatenate((X_fea[r], X_fea[c]), axis=1)
+def preprocess_labels(interaction_fpath):
+    interaction_matrix = np.loadtxt(interaction_fpath,dtype=float,delimiter=",")
+    r, c = np.triu_indices(len(interaction_matrix),1) # take indices off the diagnoal by 1
+    return interaction_matrix[r,c]
+
+def get_stratified_partitions(ddi_datatensor, num_folds=5, valid_set_portion=0.1, random_state=42):
+    """Generate 5-fold stratified sample of drug-pair ids based on the interaction label
+
+    Args:
+        ddi_datatensor: instance of :class:`DDIDataTensor`
+    """
+    skf_trte = StratifiedKFold(n_splits=num_folds, random_state=random_state, shuffle=True)  # split train and test
+    data_partitions = {}
+    X = ddi_datatensor.X_feat
+    y = ddi_datatensor.y
+    fold_num = 0
+    for train_index, test_index in skf_trte.split(X,y):
+    
+        data_partitions[fold_num] = {'train': train_index,
+                                     'test': test_index}
+        print("fold_num:", fold_num)
+        print('train data')
+        report_label_distrib(y[train_index])
+        print('test data')
+        report_label_distrib(y[test_index])
+        print()
+        fold_num += 1
+        print("-"*25)
+    return(data_partitions)
+
+
+def report_label_distrib(labels):
+    classes, counts = np.unique(labels, return_counts=True)
+    norm_counts = counts/counts.sum()
+    for i, label in enumerate(classes):
+        print("class:", label, "norm count:", norm_counts[i])
+
+
+def validate_partitions(data_partitions, drugpairs_ids, valid_set_portion=0.1, test_set_portion=0.2):
+    if(not isinstance(drugpairs_ids, set)):
+        drugpairs_ids = set(drugpairs_ids)
+    num_pairs = len(drugpairs_ids)
+    test_set_accum = set([])
+    for fold_num in data_partitions:
+        print('fold_num', fold_num)
+        tr_ids = data_partitions[fold_num]['train']
+        te_ids = data_partitions[fold_num]['test']
+
+        tr_te = set(tr_ids).intersection(te_ids)
+        # assert there is no overlap among train and test partition within a fold
+        assert len(tr_te) == 0
+        print('expected test set size:', test_set_portion*num_pairs, '; actual test set size:', len(te_ids))
+        print()
+        assert np.abs(test_set_portion*num_pairs - len(te_ids)) <= 2
+        test_set_accum = test_set_accum.union(te_ids)
+    # verify that assembling test sets from each of the five folds would be equivalent to all drugpair ids
+    assert len(test_set_accum) == num_pairs
+    assert test_set_accum == drugpairs_ids
+    print("passed intersection and overlap test (i.e. train, validation and test sets are not",
+          "intersecting in each fold and the concatenation of test sets from each fold is",
+          "equivalent to the whole dataset)")
+
+
+def generate_partition_datatensor(ddi_datatensor, data_partitions):
+    datatensor_partitions = {}
+    for fold_num in data_partitions:
+        datatensor_partitions[fold_num] = {}
+        for dsettype in data_partitions[fold_num]:
+            target_ids = data_partitions[fold_num][dsettype]
+            datatensor_partition = PartitionDataTensor(ddi_datatensor, target_ids, dsettype, fold_num)
+            datatensor_partitions[fold_num][dsettype] = datatensor_partition
+    return(datatensor_partitions)
+
+def build_datatensor_partitions(data_partitions, ddi_datatensor):
+    datatensor_partitions = generate_partition_datatensor(ddi_datatensor, data_partitions)
+    compute_class_weights_per_fold_(datatensor_partitions)
+    return datatensor_partitions
+
+def compute_class_weights(labels_tensor):
+    classes, counts = np.unique(labels_tensor, return_counts=True)
+    # print("classes", classes)
+    # print("counts", counts)
+    class_weights = compute_class_weight('balanced', classes, labels_tensor.numpy())
+    return class_weights
+
+
+def compute_class_weights_per_fold_(datatensor_partitions):
+    """computes inverse class weights and updates the passed dictionary
+
+    Args:
+        datatensor_partitions: dictionary, {fold_num, int: {datasettype, string:{datapartition, instance of
+        :class:`PartitionDataTensor`}}}}
+
+    Example:
+        datatensor_partitions
+            {0: {'train': <neural.dataset.PartitionDataTensor at 0x1cec95c96a0>,
+                 'validation': <neural.dataset.PartitionDataTensor at 0x1cec95c9208>,
+                 'test': <neural.dataset.PartitionDataTensor at 0x1cec95c9240>
+                }, ..
+            }
+        is updated after computation of class weights to
+            {0: {'train': <neural.dataset.PartitionDataTensor at 0x1cec95c96a0>,
+                 'validation': <neural.dataset.PartitionDataTensor at 0x1cec95c9208>,
+                 'test': <neural.dataset.PartitionDataTensor at 0x1cec95c9240>,
+                 'class_weights': tensor([0.6957, 1.7778]),
+                 }, ..
+            }
+    """
+
+    for fold_num in datatensor_partitions:  # looping over the numbered folds
+        dpartition = datatensor_partitions[fold_num]['train']
+        partition_ids = dpartition.partition_ids
+        labels = dpartition.ddi_datatensor.y[partition_ids]
+        datatensor_partitions[fold_num]['class_weights'] = torch.from_numpy(compute_class_weights(labels)).float()
+
+def read_pickles(data_dir, device):
+
+    # Read stored data structures
+    data_partitions = ReaderWriter.read_data(os.path.join(data_dir, 'data_partitions.pkl'))
+    # instance of :class:`DDIDataTensor`
+    ddi_datatensor = ReaderWriter.read_tensor(os.path.join(data_dir, 'ddi_datatensor.torch'), device)
+
+    return data_partitions, ddi_datatensor
---
+++ ddi/utilities.py
@@ -0,0 +1,330 @@
+import os
+import shutil
+import pickle
+import torch
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report, f1_score, roc_curve, precision_recall_curve, accuracy_score, \
+                            recall_score, precision_score, roc_auc_score, auc
+from matplotlib import pyplot as plt
+
+
+class ModelScore:
+    def __init__(self, best_epoch_indx, s_auc, s_aupr, s_f1, s_precision, s_recall):
+        self.best_epoch_indx = best_epoch_indx
+        self.s_auc = s_auc
+        self.s_aupr = s_aupr
+        self.s_f1 = s_f1
+        self.s_precision = s_precision
+        self.s_recall = s_recall
+
+
+    def __repr__(self):
+        desc = " best_epoch_indx:{}\n auc:{} \n apur:{} \n f1:{} \n precision:{} \n recall:{} \n" \
+               "".format(self.best_epoch_indx, self.s_auc, self.s_aupr, self.s_f1, self.s_precision, self.s_recall)
+        return desc
+
+def get_performance_results(similarity_type, target_dir, num_folds, dsettype):
+    all_perf = {}
+    num_metrics = 3 # number of metrics to focus on
+    perf_dict = [{} for i in range(num_metrics)]  # track auc, aupr, f1 measure
+    if dsettype == 'train':
+        prefix = 'train_val'
+    else:
+        prefix = dsettype
+    for fold_num in range(num_folds):
+
+        fold_dir = os.path.join(target_dir,
+                                '{}_{}'.format(prefix, similarity_type),
+                                'fold_{}'.format(fold_num))
+
+        score_file = os.path.join(fold_dir, 'score_{}.pkl'.format(dsettype))
+        if os.path.isfile(score_file):
+            mscore = ReaderWriter.read_data(score_file)
+            perf_dict[0]['fold{}'.format(fold_num)] = mscore.s_auc
+            perf_dict[1]['fold{}'.format(fold_num)] = mscore.s_aupr
+            perf_dict[2]['fold{}'.format(fold_num)] = mscore.s_f1
+    perf_df = []
+    for i in range(num_metrics):
+        all_perf = perf_dict[i]
+        all_perf_df = pd.DataFrame(all_perf, index=[similarity_type])
+        median = all_perf_df.median(axis=1)
+        mean = all_perf_df.mean(axis=1)
+        stddev = all_perf_df.std(axis=1)
+        all_perf_df['mean'] = mean
+        all_perf_df['median'] = median
+        all_perf_df['stddev'] = stddev
+        perf_df.append(all_perf_df.sort_values('mean', ascending=False))
+    return perf_df
+
+
+def build_performance_dfs(similarity_types, target_dir, num_folds, dsettype):
+    auc_df = pd.DataFrame()
+    aupr_df = pd.DataFrame()
+    f1_df = pd.DataFrame()
+    for sim_type in similarity_types:
+        s_auc , s_aupr, s_f1 = get_performance_results(sim_type, target_dir, num_folds, dsettype)
+        auc_df = pd.concat([auc_df, s_auc], sort=True)
+        aupr_df = pd.concat([aupr_df, s_aupr], sort=True)
+        f1_df = pd.concat([f1_df, s_f1], sort=True)
+
+    return auc_df, aupr_df, f1_df
+
+
+class ReaderWriter(object):
+    """class for dumping, reading and logging data"""
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def dump_data(data, file_name, mode="wb"):
+        """dump data by pickling
+           Args:
+               data: data to be pickled
+               file_name: file path where data will be dumped
+               mode: specify writing options i.e. binary or unicode
+        """
+        with open(file_name, mode) as f:
+            pickle.dump(data, f)
+
+    @staticmethod
+    def read_data(file_name, mode="rb"):
+        """read dumped/pickled data
+           Args:
+               file_name: file path where data will be dumped
+               mode: specify writing options i.e. binary or unicode
+        """
+        with open(file_name, mode) as f:
+            data = pickle.load(f)
+        return(data)
+
+    @staticmethod
+    def dump_tensor(data, file_name):
+        """
+        Dump a tensor using PyTorch's custom serialization. Enables re-loading the tensor on a specific gpu later.
+        Args:
+            data: Tensor
+            file_name: file path where data will be dumped
+        Returns:
+        """
+        torch.save(data, file_name)
+
+    @staticmethod
+    def read_tensor(file_name, device):
+        """read dumped/pickled data
+           Args:
+               file_name: file path where data will be dumped
+               device: the gpu to load the tensor on to
+        """
+        data = torch.load(file_name, map_location=device)
+        return data
+
+    @staticmethod
+    def write_log(line, outfile, mode="a"):
+        """write data to a file
+           Args:
+               line: string representing data to be written out
+               outfile: file path where data will be written/logged
+               mode: specify writing options i.e. append, write
+        """
+        with open(outfile, mode) as f:
+            f.write(line)
+
+    @staticmethod
+    def read_log(file_name, mode="r"):
+        """write data to a file
+           Args:
+               line: string representing data to be written out
+               outfile: file path where data will be written/logged
+               mode: specify writing options i.e. append, write
+        """
+        with open(file_name, mode) as f:
+            for line in f:
+                yield line
+
+
+def create_directory(folder_name, directory="current"):
+    """create directory/folder (if it does not exist) and returns the path of the directory
+       Args:
+           folder_name: string representing the name of the folder to be created
+       Keyword Arguments:
+           directory: string representing the directory where to create the folder
+                      if `current` then the folder will be created in the current directory
+    """
+    if directory == "current":
+        path_current_dir = os.path.dirname(__file__)  # __file__ refers to utilities.py
+    else:
+        path_current_dir = directory
+    path_new_dir = os.path.join(path_current_dir, folder_name)
+    if not os.path.exists(path_new_dir):
+        os.makedirs(path_new_dir)
+    return(path_new_dir)
+
+
+def get_device(to_gpu, index=0):
+    is_cuda = torch.cuda.is_available()
+    if(is_cuda and to_gpu):
+        target_device = 'cuda:{}'.format(index)
+    else:
+        target_device = 'cpu'
+    return torch.device(target_device)
+
+
+def report_available_cuda_devices():
+    if(torch.cuda.is_available()):
+        n_gpu = torch.cuda.device_count()
+        print('number of GPUs available:', n_gpu)
+        for i in range(n_gpu):
+            print("cuda:{}, name:{}".format(i, torch.cuda.get_device_name(i)))
+            device = torch.device('cuda', i)
+            get_cuda_device_stats(device)
+            print()
+    else:
+        print("no GPU devices available!!")
+
+def get_cuda_device_stats(device):
+    print('total memory available:', torch.cuda.get_device_properties(device).total_memory/(1024**3), 'GB')
+    print('total memory allocated on device:', torch.cuda.memory_allocated(device)/(1024**3), 'GB')
+    print('max memory allocated on device:', torch.cuda.max_memory_allocated(device)/(1024**3), 'GB')
+    print('total memory cached on device:', torch.cuda.memory_cached(device)/(1024**3), 'GB')
+    print('max memory cached  on device:', torch.cuda.max_memory_cached(device)/(1024**3), 'GB')
+
+def get_interaction_stat(matrix):
+    w, h = matrix.shape
+    totalnum_elements = w*h
+    nonzero_elem = np.count_nonzero(matrix)
+    zero_elem = totalnum_elements - nonzero_elem
+    print('number of rows: {}, cols: {}'.format(w, h))
+    print('total number of elements', totalnum_elements)
+    print('number of nonzero elements', nonzero_elem)
+    print('number of zero elements', zero_elem)
+    print('diagnoal elements ', np.diag(matrix))
+
+def perfmetric_report(pred_target, ref_target, probscore, epoch, outlog, plot_roc=True):
+
+    # print(ref_target.shape)
+    # print(pred_target.shape)
+    #
+    # print("ref_target \n", ref_target)
+    # print("pred_target \n", pred_target)
+    
+
+    lsep = "\n"
+    report = "Epoch: {}".format(epoch) + lsep
+    report += "Classification report on all events:" + lsep
+    report += str(classification_report(ref_target, pred_target)) + lsep
+    report += "macro f1:" + lsep
+    macro_f1 = f1_score(ref_target, pred_target, average='macro')
+    report += str(macro_f1) + lsep
+    report += "micro f1:" + lsep
+    micro_f1 = f1_score(ref_target, pred_target, average='micro')
+    report += str(micro_f1) + lsep
+    report += "accuracy:" + lsep
+    accuracy = accuracy_score(ref_target, pred_target)
+    report += str(accuracy) + lsep
+        
+    s_auc = roc_auc_score(ref_target, probscore)
+    report += "AUC:\n" + str(s_auc) + lsep
+    precision_scores, recall_scores, __ = precision_recall_curve(ref_target, probscore)
+    s_aupr = auc(recall_scores, precision_scores)
+    report += "AUPR:\n" + str(s_aupr) + lsep
+    s_f1 = f1_score(ref_target, pred_target)
+    report += "binary f1:\n" + str(s_f1) + lsep
+    s_recall = recall_score(ref_target, pred_target)
+    s_precision = precision_score(ref_target, pred_target)
+    report += "-"*30 + lsep
+
+    modelscore = ModelScore(epoch, s_auc, s_aupr, s_f1, s_precision, s_recall)
+    ReaderWriter.write_log(report, outlog)
+    return modelscore
+
+
+def plot_precision_recall_curve(ref_target, prob_poslabel, figname, outdir):
+    pr, rec, thresholds = precision_recall_curve(ref_target, prob_poslabel)
+    thresholds[0] = 1
+    plt.figure(figsize=(9, 6))
+    plt.plot(pr, rec, 'bo', label='Precision vs Recall')
+    # plt.plot(np.arange(0,len(thresholds)), thresholds, 'r-', label='thresholds')
+    plt.xlabel('Precision')
+    plt.ylabel('Recall')
+    plt.title('Precision vs. recall curve')
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(outdir, os.path.join('precisionrecall_curve_{}'.format(figname) + ".pdf")))
+    plt.close()
+
+
+def plot_roc_curve(ref_target, prob_poslabel, figname, outdir):
+    fpr, tpr, thresholds = roc_curve(ref_target, prob_poslabel)
+    thresholds[0] = 1
+    plt.figure(figsize=(9, 6))
+    plt.plot(fpr, tpr, 'bo', label='TPR vs FPR')
+    plt.plot(fpr, thresholds, 'r-', label='thresholds')
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.savefig(os.path.join(outdir, os.path.join('roc_curve_{}'.format(figname) + ".pdf")))
+    plt.close()
+
+
+def plot_loss(epoch_loss_avgbatch, epoch_loss_avgsamples, wrk_dir):
+    dsettypes = epoch_loss_avgbatch.keys()
+    for dsettype in dsettypes:
+        plt.figure(figsize=(9, 6))
+        plt.plot(epoch_loss_avgbatch[dsettype], 'r', epoch_loss_avgsamples[dsettype], 'b')
+        plt.xlabel("number of epochs")
+        plt.ylabel("negative loglikelihood cost")
+        plt.legend(['epoch batch average loss', 'epoch training samples average loss'])
+        plt.savefig(os.path.join(wrk_dir, os.path.join(dsettype + ".pdf")))
+        plt.close()
+
+
+def delete_directory(directory):
+    if(os.path.isdir(directory)):
+        shutil.rmtree(directory)
+
+
+# code from keras https://github.com/keras-team/keras/blob/master/keras/utils/np_utils.py
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """Converts a class vector (integers) to binary class matrix.
+    E.g. for use with categorical_crossentropy.
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+        dtype: The data type expected by the input, as a string
+            (`float32`, `float64`, `int32`...)
+    # Returns
+        A binary matrix representation of the input. The classes axis
+        is placed last.
+    # Example
+    ```python
+    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
+    > labels
+    array([0, 2, 1, 2, 0])
+    # `to_categorical` converts this into a matrix with as many
+    # columns as there are classes. The number of rows
+    # stays the same.
+    > to_categorical(labels)
+    array([[ 1.,  0.,  0.],
+           [ 0.,  0.,  1.],
+           [ 0.,  1.,  0.],
+           [ 0.,  0.,  1.],
+           [ 1.,  0.,  0.]], dtype=float32)
+    ```
+    """
+
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
